e7fa97a79b6dd1ae2390d08c18bc696569d229d5
it21371790 <99164099+it21371790@users.noreply.github.com>
2024-07-04 15:59:13 +0530

    Initial commit

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { AiOutlineDownload } from "react-icons/ai";
+
+import Particle from '../components/Particle'
+
+const Resume = () => {
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href="https://raw.githubusercontent.com/it20207540/portfolio_Naween_Live/master/src/assets/cv.pdf" // Updated URL
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download Resume
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Resume;
